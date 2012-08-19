7f38e60414c5e1f793fdbe5014f0cf96928a9715
Aider Ibragimov <aider.ibragimov@gmail.com>
2012-08-19 22:35:43 +0300

    Replaced Command-T with CtrlP, removed old bindings

--- vim/install.sh
+++ vim/install.sh
@@ -1,10 +1,5 @@
 sudo apt-get install vim vim-rails
 sudo apt-get install exuberant-ctags
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-touch ~/.vimrc
-echo "~/dotfiles/vim/vimrc" > ~/.vimrc
+cp ~/dotfiles/vim/vimrc ~/.vimrc
 vim +BundleInstall +qall
-cd ~/.vim/bundle/Command-T/ruby/command-t
-ruby extconf.rb
-make
-cd ~
