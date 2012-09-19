bb244eb65ccad55e00baaf7179c82e4bedde6ce8
Aider Ibragimov <aider.ibragimov@gmail.com>
2012-09-19 23:18:18 +0300

    Add fancy mod for powerline and support xterm_clipboard

--- vim/install.sh
+++ vim/install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install vim vim-rails
+sudo apt-get install vim vim-rails vim-gnome
 sudo apt-get install exuberant-ctags
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 cp dotfiles/vim/vimrc ~/.vimrc
