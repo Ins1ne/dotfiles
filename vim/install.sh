8a930ea010d370f637153978b4349d87f123dac5
Aider Ibragimov <aider.ibragimov@gmail.com>
2015-05-05 11:41:15 +0300

    fix install script

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,5 @@
-sudo apt-get install vim vim-rails vim-gnome
+sudo apt-get install vim vim-gnome
 sudo apt-get install exuberant-ctags
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-cp dotfiles/vim/vimrc ~/.vimrc
+cp vim/vimrc ~/.vimrc
 vim +BundleInstall +qall
